946c2920e7109e7f87e6752ec9d6bffa0daf09a8
yshashanky <812shanky@gmail.com>
2023-03-16 01:59:08 +0530

    update

--- network.py
+++ network.py
@@ -6,8 +6,10 @@ class Network:
         self.server = "192.168.1.42"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.id = self.connect()
-        print(self.id)
+        self.pos = self.connect()
+    
+    def getPos(self):
+        return self.pos
 
     def connect(self):
         try:
@@ -22,6 +24,3 @@ class Network:
             return self.client.recv(2048).decode()
         except:
             pass
-
-n = Network()
-print(n.send("hellow"))
--- server.py
+++ server.py
@@ -12,26 +12,39 @@ try:
 except socket.error as e:
     str(e)
 
-
 s.listen(2)
 print("Waiting for connection, server started...")
 
-def threader_client(conn):
-    conn.send(str.encode("connected"))
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+pos = [(0,0), (100,100)]
+
+def threader_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
+            data = read_pos(conn.recv(2048).decode("utf-8"))
+            pos[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
                 print(f"Received: {reply}")
                 print(f"Sending: {reply}")
             
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
 
         except:
             break
@@ -39,8 +52,11 @@ def threader_client(conn):
     print("Lost connection")
     conn.close()
 
+currentPlayer = 0 
+
 while True:
     conn, addr = s.accept()
     print(f"Connected to: {addr}")
 
-    start_new_thread(threader_client, (conn, ))
+    start_new_thread(threader_client, (conn, currentPlayer ))
+    currentPlayer +=1
